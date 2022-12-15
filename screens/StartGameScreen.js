0460ea0b7a67ed56c228a5216b147a6f456e8d61
ciarancooke9 <ciaran.cooke9@mail.dcu.ie>
2022-12-15 14:45:11 +0000

    style added to start game screen

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,10 +1,10 @@
-import { TextInput, View } from "react-native";
+import { TextInput, View, StyleSheet } from "react-native";
 
 import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen() {
   return (
-    <View>
+    <View style={styles.inputContainer}>
       <TextInput />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Submit</PrimaryButton>
@@ -13,3 +13,11 @@ function StartGameScreen() {
 }
 
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    flex: 1,
+    padding: 16,
+    marginTop: 100,
+  }
+})
