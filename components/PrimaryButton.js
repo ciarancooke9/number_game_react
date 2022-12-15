fe99300ca4baf61b65edddab02199ee8294023b7
ciarancooke9 <ciaran.cooke9@mail.dcu.ie>
2022-12-15 16:17:11 +0000

    Buttons styles

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,10 +1,47 @@
-import { Text, View } from "react-native";
+import { Text, View, Pressable, StyleSheet } from "react-native";
 
-function PrimaryButton({ children }) { //takes button text as prop
-  return(
-  <View>
-    <Text>{children}</Text>
-  </View>);
+function PrimaryButton({ children }) {
+  //takes button text as prop
+  function pressHandler(params) {
+    console.log("pressed");
+  }
+
+  return (
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressed]
+            : styles.buttonInnerContainer
+        }
+        onPress={pressHandler}
+        android_ripple={{ color: "#640233" }}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
+    </View>
+  );
 }
 
 export default PrimaryButton;
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: "hidden",
+  },
+  buttonInnerContainer: {
+    backgroundColor: "#72063c",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -5,7 +5,12 @@ import PrimaryButton from "../components/PrimaryButton";
 function StartGameScreen() {
   return (
     <View style={styles.inputContainer}>
-      <TextInput />
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+      />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Submit</PrimaryButton>
     </View>
@@ -16,8 +21,26 @@ export default StartGameScreen;
 
 const styles = StyleSheet.create({
   inputContainer: {
-    flex: 1,
     padding: 16,
     marginTop: 100,
-  }
-})
+    marginHorizontal: 24,
+    backgroundColor: "#4e0329",
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 6,
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
