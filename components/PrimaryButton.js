bdb0eac948b39c6c6b772125c6a3685a11080e66
ciarancooke9 <ciaran.cooke9@mail.dcu.ie>
2022-12-15 14:23:36 +0000

    Update PrimaryButton.js
    comment added to primary button

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,6 +1,6 @@
 import { Text, View } from "react-native";
 
-function PrimaryButton({ children }) {
+function PrimaryButton({ children }) { //takes button text as prop
   return(
   <View>
     <Text>{children}</Text>
